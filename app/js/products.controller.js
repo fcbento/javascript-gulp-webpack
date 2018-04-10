3af4b23ac550b98a1bffe3f0b87b142c6789fae6
Felipe Bento <felipe.16costa@gmail.com>
2018-04-10 17:29:05 -0300

    Loads of changes

--- app/js/products.controller.js
+++ app/js/products.controller.js
@@ -7,9 +7,8 @@ class ProductController {
     displayPotions() {
         const potions = pm.getData();
         potions.forEach((potion) => {
-            console.log(potion)
             const allPotionsDom = document.querySelector('#allPotions');
-            allPotionsDom.innerHTML += `<div class="col-xs-4"> <a id="detailed_${potion.id}"> ${potion.name} </a> </div>`
+            allPotionsDom.innerHTML += `<div class="col-xs-4"> <img id="potionImg" src="assets/${potion.image}" alt="Potion name"> <p id="detailed_${potion.id}"> ${potion.name} - <span>$ ${potion.price} </span></p> </div>`
         });
         this.detail(potions)
     }
@@ -28,16 +27,20 @@ class ProductController {
     openModal(potion) {
         const modal = document.getElementById('myModal');
         const close = document.getElementsByClassName('close')[0];
-       
+
         let name = document.getElementById('name');
         let effect = document.getElementById('effect');
         let price = document.getElementById('price');
-        let ingredients = document.getElementById('ingredients');
-    
+        let image = document.getElementById('image').src = `assets/${potion.image}`;
+
         name.textContent = potion.name;
         effect.textContent = potion.effect;
-        price.textContent = potion.price;
-        ingredients.textContent += potion.ingredients;
+        price.textContent = `$ ${potion.price};`
+
+        potion.ingredients.forEach((ingredient) => {
+            let ingredientsDom = document.getElementById('ingredients');
+            ingredientsDom.innerHTML += `<p id="ingredientm">${ingredient}</p>`;
+        });
 
         modal.style.display = 'block';
         close.addEventListener('click', () => {
